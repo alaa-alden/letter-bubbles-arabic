d935dce7feec873d277f1531cbc6c04d4d0d2e9d
alaa-alden <alaa.alden.010@gmail.com>
2018-06-16 03:07:25 +0300

    good work in this day 2018/6/16

--- src/class/letter.js
+++ src/class/letter.js
@@ -4,11 +4,7 @@ import {
     canvasDraw,
     canvas
 } from '../config/canvas'
-/*import {
-    distance,
-    resolveCollision
-} from '../function'
-*/
+
 class Letter {
     constructor(old,show, velocity, x, y, radius, colorCircle, letter, xLetter, yLetter, colorLetter) {
         this.old=old
@@ -27,35 +23,8 @@ class Letter {
         this.draw = this.draw.bind(this)
 
     }
-    update(Letters) {
-
-        /*// for collision
-        for (let i = 0; i < Letters.length; i++) {
-            if (this === Letters[i]) continue
-            if (distance(this.x, this.y, Letters[i].x, Letters[i].y) - this.radius * 2 < 0)
-                resolveCollision(this, Letters[i])
-        }*/
-        // if letter arrive to scanner left
-        if (this.x - this.radius < 0 || this.x - this.radius > canvas.width+10) {
-            /*let l = 0,
-                r = Letters.length,
-                mid
-            while (l <= r) {
-                mid = l + r >> 1
-                if (Letters[mid].id < this.id) l = mid + 1
-                else if (Letters[mid].id > this.id) r = mid - 1
-                else if (Letters[mid].id == this.id) {
-                    Letters.splice(mid, 1)
-                    console.log("when delete left/far right " + Letters.length)
-                }
-            }*/
-            // Letters.splice(index, 1)
-            this.show=false
-            if (this.colorCircle == '#71DDE3')
-                console.log('when delete left/far right ' + Letters.length)
-        }
-        // this.velocity.x = -this.velocity.x
-        else if (this.y - this.radius <= 0 || this.y + this.radius >= canvas.height)
+    update() {
+        if (this.y - this.radius <= 0 || this.y + this.radius >= canvas.height)
             this.velocity.y = -this.velocity.y
         // change coordinate for circle center
         this.x += this.velocity.x
--- src/index.js
+++ src/index.js
@@ -9,154 +9,127 @@ import {
     canvas,
     canvasDraw
 } from './config/canvas'
-
-/*const colors = ['#E37B40', '#46B29D', '#46B29D', '#324D5C', '#F0CA4D', '#F18229',
-    '#458955', '#EDD569', '#3F628F', '#458955', '#E94128',
-]*/
-//const colors = ['#59A2E8', '#EDD569', '#FF8E80', '#FFFFFF', '#E94128', '#F18229', '#458955', '#FEBEBC', '#F45265']
-
 let
     chars = ['أ', 'ب', 'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي', 'ا', 'إ', 'آ', 'ؤ', 'ئ', 'ء'],
     idxy = [15, 8, 8, 8, 5, 5, 5, 10, 10, 5, 5, 5, 5, 5, 5, 10, 10, 5, 5, 10, 5, 13, 10, 5, 5, 5, 5, 5, 15, 15, 15, 5, 5, 10],
-    letters, numberletters = 0
+    letters, numberletters = 0,
+    CounterError = 0,
+    CounterLeft = 0,
+    speed = 5,
+    devspeed = 0.5,
+    MaxNumberLetter = 15,
+    step,
+    win = true
+var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
+var cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame
 
+
+// may we don't need this
 // update size when change size of window
 addEventListener('resize', () => {
-    canvas.width = 2*innerWidth/3
-    canvas.height = 2*innerHeight/3
-    init()
+    canvas.width = 2 * innerWidth / 3
+    canvas.height = 2 * innerHeight / 3
+    //init()
 })
-let CounterError = 0,
-    CounterLeft = 0
 // check if key is same any letter on scaner for delete
 addEventListener('keydown', function (event) {
     // console.log(event)
-    let check=false
+    let check = false
     for (let i in letters) {
         if (event.key == letters[i].letter) {
-            // letters.splice(i, 1)
-            //letters[i].show=false
             letters[i].colorCircle = '#F01A30'
-            console.log('when delete >>> ' + letters.length)
-            check=true
+            check = true
         }
     }
-    if(check)CounterError++
+    if (!check) CounterError++
+
 })
 // Implementation
 
 function init() {
     letters = []
+    // call animaltion letter
+    animate()
+
+}
+// Animation Loop , this animation letter by update fun of letters
+function animate() {
+    // clear canvas for new draw,when update coordinate letters or not show some letters
+    canvasDraw.clearRect(0, 0, canvas.width, canvas.height)
+    if (numberletters == MaxNumberLetter) { //win
+        clearInterval(createLetter)
+        win = true
+        // check if create all letters and all letters caught
+        letters.forEach((letter) => {
+            if (letter.show && letter.colorCircle == '#00A5FF') win = false
+        })
+        if (win) {
+            canvasDraw.font = '40px arial',
+            canvasDraw.textAlign = 'center',
+            canvasDraw.fillText('you have won in our Game', canvas.width / 2, canvas.height / 2)
+            cancelAnimationFrame(step)
+            return 0
+        }
+    }
+
+    if (CounterLeft == 5) { // check if five letters arrive to canvas's left wihtout catching them
+        canvasDraw.font = '40px arial',
+        canvasDraw.textAlign = 'center',
+        canvasDraw.fillText('good luck in the future', canvas.width / 2, canvas.height / 2)
+        cancelAnimationFrame(step)
+        clearInterval(createLetter)
+    } else {
+        //else coutinue with Game
+        letters.forEach(letter => {
+            // check if letter arrive to scanner left
+            if (letter.show // letter include canvas
+                &&
+                letter.colorCircle == '#00A5FF' // not catch
+                &&
+                letter.x - letter.radius < 0 //arrive left 
+            ) {
+                CounterLeft++
+                letter.show = false
+            }
+
+            //update or  catch one of letters , start it big then not show 
+            if (letter.show) {
+                if (letter.colorCircle == '#F01A30')
+                    letter.old++, letter.radius += 0.5
+                if (letter.old == 25)
+                    letter.show = false
+                else
+                    letter.update()
+            }
+        })
+    }
+    document.getElementById('Countererror').innerHTML = CounterError
+    document.getElementById('Counterleft').innerHTML = 5 - CounterLeft
+    step = requestAnimationFrame(animate)
+}
+// create letter each 1 second
+var createLetter = setInterval(() => {
+    // creater properties for new letter
     let velocity = {
-            x: -(Math.abs((Math.random() - 0.5) * 5)),
+            x: -(Math.abs((Math.random() - 0.5) * speed)),
             y: (Math.random() - 0.5) * 5
-        } // for speed letter
+        } //for speed letter
         ,
         radius = 30,
-        // x = randomIntFromRange(radius, canvas.width - radius),
         x = canvas.width + 10,
         y = randomIntFromRange(radius, canvas.height - radius),
-        //indexColor = randomIndex(colors),
+        // indexColor = randomIndex(colors),
         indexChar = randomIndex(chars),
         ychar = idxy[indexChar]
     numberletters++
-    letters.push(new Letter(0, true, velocity, x, y, radius, '#71DDE3', chars[indexChar], x, y + ychar, 'black'))
-    //colors[indexColor], chars[indexChar], x, y + ychar, 'black'))
-}
-
-
-// Animation Loop , this animation letter by update fun of letters
-function animate() {
-    canvasDraw.clearRect(0, 0, canvas.width, canvas.height)
-    if (CounterLeft == 5) canvasDraw.font = '40px arial',
-    canvasDraw.textAlign = 'center',
-    canvasDraw.fillText('good luck in the future', canvas.width/2, canvas.height/2)
-    else {
-        requestAnimationFrame(animate)
-        letters.forEach(letter => {
-            if (letter.colorCircle == '#F01A30')
-                letter.old++,letter.radius+=0.5
-            if(letter.old==25)
-                letter.show=false
-            else if(letter.show)
-                letter.update(letters)
-            // check if letter arrive to scanner left
-            if (letter.x - letter.radius < 0 || letter.x - letter.radius > canvas.width + 10) {  
-                if (letter.show){
-                    if (letter.x - letter.radius < 0) CounterLeft++
-                    if (letter.colorCircle == '#71DDE3')
-                        console.log('when delete left/far right ' + letters.length)
-                }
-                letter.show = false
-
-            }
-        })
-       
-    }
-
-}
-
-// create letter each 1 second
-// this code for increase speed 
-let speed = 5,
-    devspeed = 0.5
-setInterval(() => {
-    // test 50 add letter ,becuase suddenly hide some letters ,and I don't why
-    if (numberletters < 25) {
-        // creater properties for new letter
-        let velocity = {
-                x: -(Math.abs((Math.random() - 0.5) * speed)),
-                y: (Math.random() - 0.5) * 5
-            } //for speed letter
-            ,
-            radius = 30,
-            x = canvas.width + 10,
-            y = randomIntFromRange(radius, canvas.height - radius),
-            // indexColor = randomIndex(colors),
-            indexChar = randomIndex(chars),
-            ychar = idxy[indexChar]
-        // check if any letter of array and new letter are same place for y-coordinate
-        for (let j = 0; j < letters.length; j++) {
-            if (distance(x, y, letters[j].x, letters[j].y) - radius * 2 < 0) {
-                // x = randomIntFromRange(radius, canvas.width - radius),
-                y = randomIntFromRange(radius, canvas.height - radius),
-                j = -1 // for back to start loop when update
-            }
-        }
-        numberletters++
-        letters.push(new Letter(0, true, velocity, x, y, radius, '#71DDE3', chars[indexChar], x, y + ychar, 'black'))
-        // colors[indexColor], chars[indexChar], x, y + ychar, 'black'))
-        console.log('new ' + letters.length)
-    }
-    console.log(letters.length)
+    letters.push(new Letter(0, true, velocity, x, y, radius, '#00A5FF', chars[indexChar], x, y + ychar, '#FCFFF5'))
     // increase speed for next letter
     if (speed < 15)
         speed += devspeed
-    else {
-        devspeed += 0.1
-        // console.log('speed',devspeed)
+    else { //speed arrive 15 so decrease devspeed
+        devspeed = 0.1
     }
-    console.log('error '+CounterError)
-    console.log('letter left '+CounterLeft)
-
-}, 800)
+}, 1000)
 //  call initialization function
 init()
-// call animaltion letter
-animate()
-
-
-
-
-// this code for increase speed 
-/*let speed = 0.9,
-    devspeed = 0.09*/
-
-// increase speed for next letter
-/* if (speed > devspeed)
-     speed -= devspeed
- else {
-     speed=0.9
-     // console.log('id',devspeed)
- }
- // console.log(speed)*/
